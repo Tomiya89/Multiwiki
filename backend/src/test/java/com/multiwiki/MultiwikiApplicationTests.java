1cfd0b9d13da14388f0c60a51c8a9a04a472f022
Tomiya <murvik2019@gmail.com>
2026-04-01 19:20:44 +0300

    build: init backend project and dependencies

---
+++ backend/src/test/java/com/multiwiki/MultiwikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multiwiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiwikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
